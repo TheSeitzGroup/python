0e9981e09a11c39a409b93d1e3a15e7635af3f74
Ryan <TheSeitzGroup@users.noreply.github.com>
2023-08-31 15:50:24 -0400

    Create Outlier_analysis.py

---
+++ Outlier_analysis.py
@@ -0,0 +1,8 @@
+'''
+Outlier analysis is a technique used to identify data points that are significantly different from the rest of the data. This can be useful for identifying errors, fraud, or other anomalies. There are a number of different methods for outlier analysis, but some of the most common include:
+
+Z-score: This method calculates the distance between each data point and the mean of the data set. Data points that are more than a certain number of standard deviations away from the mean are considered to be outliers.
+IQR: This method calculates the interquartile range of the data set. Data points that are below the first quartile or above the third quartile are considered to be outliers.
+Tukey's fences: This method calculates three fences, one at the 1.5th quantile and one at the 3rd quantile. Data points that are below the first fence or above the third fence are considered to be outliers.
+Once outliers have been identified, they can be removed from the data set or treated in some other way. For example, they may be replaced with the mean of the data set, or they may be flagged as suspicious.
+'''
